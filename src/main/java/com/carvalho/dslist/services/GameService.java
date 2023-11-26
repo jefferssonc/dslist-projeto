1018fb71593349b03b683ce4b248ecb7dcd5c64d
jefferssonc <92582232+jefferssonc@users.noreply.github.com>
2023-11-26 13:43:36 -0300

    GamesListController, GameMinDTO, etc

--- src/main/java/com/carvalho/dslist/services/GameService.java
+++ src/main/java/com/carvalho/dslist/services/GameService.java
@@ -4,12 +4,15 @@ package com.carvalho.dslist.services;
 import com.carvalho.dslist.dto.GameDTO;
 import com.carvalho.dslist.dto.GameMinDTO;
 import com.carvalho.dslist.entities.Game;
+import com.carvalho.dslist.projections.GameMinProjection;
 import com.carvalho.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static java.util.Arrays.stream;
 
 @Service
 public class GameService {
@@ -22,6 +25,13 @@ public class GameService {
         Game result = gameRepository.findById(id).get();
         GameDTO dto = new GameDTO(result);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        var result = gameRepository.searchByList(listId);
+        //List<GameMinProjection> dto =
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
     @Transactional(readOnly = true)
